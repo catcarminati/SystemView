0f2ab5e83f72fa70f8d001fd11ccbdfeb0e8c240
catcarminati <catherine00023@gmail.com>
2022-03-16 09:14:35 +0200

    Refactoring requests movement

---
+++ src/Assets/TechInc/Scripts/ApplicationManager.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class ApplicationManager : MonoBehaviour
+{
+    public Dictionary<string,GameObject> Applications = new Dictionary<string, GameObject>();
+
+    public GameObject App;
+    public RequestObject Request;
+
+    public RectTransform panel;
+    private List<RequestObject> Requests = new List<RequestObject>();
+
+    public int NumRequests = 1;
+    // Start is called before the first frame update
+    async void Start()
+    {
+        // TODO get real data
+        var newRequest = new RequestObject();
+        newRequest.DestinationAppName = "Server";
+        newRequest.CallingApplicationName = "Veyron";
+        newRequest.Key = 1; // this needs to be unique
+
+        Requests.Add(newRequest);
+
+        var newRequest2 = new RequestObject();
+        newRequest2.DestinationAppName = "AccountAPI";
+        newRequest2.CallingApplicationName = "Veyron";
+        newRequest2.Key = 2; // this needs to be unique
+
+        Requests.Add(newRequest2);
+
+        var newRequest3 = new RequestObject();
+        newRequest3.DestinationAppName = "BalanceAPI";
+        newRequest3.CallingApplicationName = "AccountAPI";
+        newRequest3.Key = 3; // this needs to be unique
+
+        Requests.Add(newRequest3);
+
+        var newRequest4 = new RequestObject();
+        newRequest4.DestinationAppName = "VanguardAPI";
+        newRequest4.CallingApplicationName = "AccountAPI";
+        newRequest4.Key = 4; // this needs to be unique
+
+        Requests.Add(newRequest4);
+
+        var newRequest5 = new RequestObject();
+        newRequest5.DestinationAppName = "AccountAPI";
+        newRequest5.CallingApplicationName = "BalanceAPI";
+        newRequest5.Key = 5; // this needs to be unique
+
+        Requests.Add(newRequest5);
+
+        var newRequest6 = new RequestObject();
+        newRequest6.DestinationAppName = "Veyron";
+        newRequest6.CallingApplicationName = "AccountAPI";
+        newRequest6.Key = 6; // this needs to be unique
+
+        Requests.Add(newRequest6);
+
+        // Spawn application game objects of the Calling / Destination applications from the requests, if the application doesn't already exist
+        foreach (var request in Requests)
+        {
+            if(!Applications.ContainsKey(request.CallingApplicationName))          
+            {
+                Vector3 spawnPosition = GetBottomLeftCorner(panel) - new Vector3(Random.Range(0, panel.rect.x), Random.Range(0, panel.rect.y), 0);
+
+                var obj = Instantiate(App, spawnPosition, Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                obj.name = request.CallingApplicationName;
+
+                Applications.Add(request.CallingApplicationName, obj);
+            }
+
+            if(!Applications.ContainsKey(request.DestinationAppName))
+            {
+                Vector3 spawnPosition = GetBottomLeftCorner(panel) - new Vector3(Random.Range(0, panel.rect.x), Random.Range(0, panel.rect.y), 0);
+
+                var obj = Instantiate(App, spawnPosition, Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                obj.name = request.DestinationAppName;
+
+                Applications.Add(request.DestinationAppName,obj);
+            }
+        }
+    }
+
+     Vector3 GetBottomLeftCorner(RectTransform rt)
+    {
+        Vector3[] v = new Vector3[4];
+        rt.GetWorldCorners(v);
+        return v[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LoadRequests(NumRequests);
+
+        for (int i = 0; i < Requests.Count; i++)
+        {
+            foreach (var appItem in Applications)
+            {
+                if(Requests[i].CallingApplicationName == appItem.Key)
+                {
+                    var reqObj = Instantiate(Request, appItem.Value.transform.position, Quaternion.identity);
+
+                    reqObj.name = Requests[i].Key.ToString();
+
+                    foreach (var app in Applications)
+                    {
+                        if(Requests[i].DestinationAppName == app.Key)
+                        {
+                            reqObj.TargetPosition = app.Value.transform.position;
+                        }
+                    }
+                }
+            }
+            Requests.RemoveAt(i);
+            i--;
+        }   
+    }
+
+      public void LoadRequests(int numRequests)
+    {
+        var randomNum1 = 0;
+        var randomNum2 = 0;
+        do
+        {
+            randomNum1 = Random.Range(0, Applications.Count);
+            randomNum2 = Random.Range(0, Applications.Count);
+        } 
+        while (randomNum1 == randomNum2);
+
+        for (int i = 0; i < numRequests; i++)
+        {
+            var newRequest = new RequestObject();
+            newRequest.DestinationAppName = Applications.Keys.ElementAt(randomNum1);
+            newRequest.CallingApplicationName = Applications.Keys.ElementAt(randomNum2);
+            newRequest.Key = Requests.Count + 1; // this needs to be unique
+
+            Requests.Add(newRequest);
+        }
+    }
+
+
+}
---
+++ src/Assets/TechInc/Scripts/RequestObject.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+public class RequestObject : MonoBehaviour
+{
+    public int Key;
+    public string CallingApplicationName;
+    public string DestinationAppName;
+    public Vector3 TargetPosition;
+
+    void Start()
+    {
+        StartCoroutine(LerpPosition(TargetPosition, 5));
+    }
+
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(transform.position == TargetPosition){
+         DestroyImmediate(gameObject);
+     }
+    }
+}
